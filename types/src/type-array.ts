a309d1b23454fc89c7782e63679b21a5c0c77958
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-08-27 20:03:26 -0300

    types - type array

---
+++ types/src/type-array.ts
@@ -0,0 +1,17 @@
+function multiplyAllNumbers(...numbers: Array<number>): number {
+  return numbers.reduce((acc, value) => acc * value, 1);
+}
+
+console.log(multiplyAllNumbers(1, 2, 3, 4, 5));
+
+function concatenateAllStrings(...words: string[]): string {
+  return words.join('');
+}
+
+console.log(concatenateAllStrings('0', 'x', 'C', 'A', 'F', 'E'));
+
+function allStringsToUpperCase(...words: string[]): Array<string> {
+  return words.map((word) => word.toUpperCase());
+}
+
+console.log(allStringsToUpperCase('one', 'two', 'three', 'four', 'five'));
