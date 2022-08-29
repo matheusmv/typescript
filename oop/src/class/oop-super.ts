18ee7ad33ce8c940cf0558b387135ddac38f8774
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-08-29 20:19:17 -0300

    oop - super

---
+++ oop/src/class/oop-super.ts
@@ -0,0 +1,39 @@
+export class Metadata {
+  private readonly createdAt: Date = new Date();
+
+  constructor(private id: number) {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  setId(id: number): void {
+    this.id = id;
+  }
+
+  getCreatedAt(): string {
+    return this.createdAt.toUTCString();
+  }
+}
+
+export class User extends Metadata {
+  constructor(
+    id: number,
+    private username: string,
+    private password: string,
+    private email: string,
+  ) {
+    super(id);
+  }
+
+  toString(): string {
+    return `id: ${super.getId()}\nusername: ${this.username}\npassword: ${
+      this.password
+    }\nemail: ${this.email}\ncreated: ${super.getCreatedAt()}`;
+  }
+}
+
+const jhon = new User(1, 'jhon', '12345', 'jhon@email.com');
+
+console.log(jhon);
+console.log(jhon.toString());
