4c0b5ba1a03f41e837ae95e9a34231718f396cc6
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-02 20:50:55 -0300

    decorators - factory

---
+++ decorators/src/decorator-factory.ts
@@ -0,0 +1,30 @@
+export function whitValues(props?: Record<string, unknown>) {
+  return function <T extends new (...args: any[]) => any>(target: T): T {
+    return class extends target {
+      constructor(...args: any[]) {
+        super(...args);
+        for (const key in props) {
+          if (!this[key]) this[key] = props[key];
+        }
+      }
+    };
+  };
+}
+
+@whitValues({
+  id: 1,
+  username: 'jhon',
+  password: '12345',
+  email: 'jhon@email.com',
+})
+export class User {
+  constructor(
+    public id?: number,
+    public username?: string,
+    public password?: string,
+    public email?: string,
+  ) {}
+}
+
+const jhon = new User();
+console.log(jhon);
