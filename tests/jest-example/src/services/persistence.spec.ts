e87515a58db5e3a9e268b144adcf21c738bf73e1
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-03 14:11:17 -0300

    tests - jest spyOn

--- tests/jest-example/src/services/persistence.spec.ts
+++ tests/jest-example/src/services/persistence.spec.ts
@@ -1,11 +1,29 @@
-const number = 1;
+import { Persistence } from './persistence';
 
-describe('testing the constant number', () => {
-  it('must have the value one', () => {
-    expect(number).toBe(1);
+describe('Persistence', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = new Persistence();
+
+    expect(sut.saveOrder()).toBeUndefined();
   });
 
-  test('must have a value greater than zero', () => {
-    expect(number).toBeGreaterThan(0);
+  it('should call console.log once', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    sut.saveOrder();
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "order saved successfully"', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    sut.saveOrder();
+
+    expect(consoleSpy).toHaveBeenCalledWith('order saved successfully');
   });
 });
