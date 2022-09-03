85b98e36e2f97260138f8cb3e643d132d2c6f8ac
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-02 22:52:02 -0300

    namespace & module - basic namespace

---
+++ namespace-module/src/namespace.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace MyApp {
+  export const port = 8080;
+
+  export class User {
+    constructor(
+      public id: number,
+      public username: string,
+      public password: string,
+    ) {}
+  }
+
+  export namespace Operations {
+    export function sum(x: number, y: number): number {
+      return x + y;
+    }
+  }
+}
+
+console.log(MyApp.port);
+
+const jhon = new MyApp.User(1, 'jhon', '12345');
+console.log(jhon);
+
+console.log(MyApp.Operations.sum(2, 2));
+
+export default 1;
