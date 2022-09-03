313447d6da0236df89fa0278ffb9ca314b0745a8
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-09-02 21:47:30 -0300

    decorators - method

---
+++ decorators/src/method-decorator.ts
@@ -0,0 +1,26 @@
+export function logMetadata(
+  classPrototype: any,
+  methodName: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(methodName);
+  console.log(descriptor);
+}
+
+export class User {
+  constructor(
+    public id?: number,
+    public username?: string,
+    public password?: string,
+    public email?: string,
+  ) {}
+
+  @logMetadata
+  toString(): string {
+    return `User [id: ${this.id}, username: ${this.username}, password: ${this.password}, email: ${this.email}]`;
+  }
+}
+
+const alex = new User(1, 'alex', '12345', 'alex@email.com');
+console.log(alex.toString());
